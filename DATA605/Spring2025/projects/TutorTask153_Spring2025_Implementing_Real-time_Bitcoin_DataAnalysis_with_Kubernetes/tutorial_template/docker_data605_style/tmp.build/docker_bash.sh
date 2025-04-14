fd1c31ea9f3ce865b3653fe390adb6143b8d8f47
Serjius Infanto <serjiusinfanto@gmail.com>
2025-04-13 20:47:38 -0400

    TutorTask153 - Completed Docker Setup

--- DATA605/Spring2025/projects/TutorTask153_Spring2025_Implementing_Real-time_Bitcoin_DataAnalysis_with_Kubernetes/tutorial_template/docker_data605_style/tmp.build/docker_bash.sh
+++ DATA605/Spring2025/projects/TutorTask153_Spring2025_Implementing_Real-time_Bitcoin_DataAnalysis_with_Kubernetes/tutorial_template/docker_data605_style/tmp.build/docker_bash.sh
@@ -9,6 +9,6 @@ docker image ls $FULL_IMAGE_NAME
 CONTAINER_NAME=$IMAGE_NAME
 docker run --rm -ti \
     --name $CONTAINER_NAME \
-    -p 9999:8888 \
-    -v "$(pwd):/data" \
+    -p 8888:8888 \
+    -v $(pwd):/data \
     $FULL_IMAGE_NAME
