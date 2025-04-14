0c244f9727688f2a56685e0bf6bc44b95d6a5ef9
Sagar Maheshwari <sagarmaheshwari@sagars-mbp.lan>
2025-04-13 20:37:21 -0400

    TutorTask96_Spring2025_Real-Time_Bitcoin_Data_Processing_with_Gensim_1

---
+++ DATA605/Spring2025/projects/TutorTask96_Spring2025_Real-Time_Bitcoin_Data_Processing_with_Gensim/data_ingestion.py
@@ -0,0 +1,50 @@
+import requests
+import pandas as pd
+import numpy as np
+import time
+from loguru import logger
+
+# FETCHING DATA FROM COINGECKO
+def fetch_price():
+    try:
+        params ={
+            'ids': 'bitcoin',
+            'vs_currencies': 'usd'
+        }
+        url = f'https://api.coingecko.com/api/v3/simple/price'
+        response = requests.get(url,params)
+        data = response.json()
+        return data['bitcoin']['usd']
+    except:
+        logger.info("Error fetching bitcoin price")
+
+# SAVING DATA IN CSV FILE
+def save(timestamp, price):
+    try:
+        df = pd.read_csv("data.csv")
+        tempdf = pd.DataFrame({'time': [timestamp], 'price': [price]})
+        tempdf['date'] = tempdf['time'].dt.date
+        tempdf['time'] = tempdf['time'].dt.time
+        df = pd.concat([df,tempdf])
+    except:
+        tempdf = pd.DataFrame({'time': [timestamp], 'price': [price]})
+        tempdf['date'] = tempdf['time'].dt.date
+        tempdf['time'] = tempdf['time'].dt.time
+        df = tempdf.copy()
+    df.to_csv("data.csv", index=False)
+
+
+
+for _ in range(1):  # Collect for x minutes
+# for _ in range(60):  # Collect for 60 minutes
+# while True:
+    timestamp = pd.Timestamp.now()
+    price = fetch_price()
+
+    if timestamp and price:
+        logger.info(f"Time: {timestamp} | Price: {price}")
+        save(timestamp, price)
+    else:
+        logger.info("No record found")
+        
+    time.sleep(60)  # Scraping data after every 60 seconds
