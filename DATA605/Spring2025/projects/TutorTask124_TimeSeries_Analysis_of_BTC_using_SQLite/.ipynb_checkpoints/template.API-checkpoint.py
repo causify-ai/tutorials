86c7d8c7fdd4c69779bf13e805cbe288781a77e1
AsutoshDalei <asutoshdalei@gmail.com>
2025-04-19 14:34:53 -0400

    added api.py script

--- DATA605/Spring2025/projects/TutorTask124_TimeSeries_Analysis_of_BTC_using_SQLite/.ipynb_checkpoints/template.API-checkpoint.py
+++ DATA605/Spring2025/projects/TutorTask124_TimeSeries_Analysis_of_BTC_using_SQLite/.ipynb_checkpoints/template.API-checkpoint.py
@@ -27,8 +27,15 @@ import logging
 # For more info check: https://docs.python.org/3/library/typing.html
 from typing import List
 
+import logging
+from requests import Request, Session
 import pandas as pd
-import numpy as np
+import sqlite3
+import mplfinance as mpf
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sqlite_utils
 
 # Prefer using logger over print statements.
 # You can use logger in the following manner:
@@ -44,47 +51,131 @@ _LOG = logging.getLogger(__name__)
 
 
 # #############################################################################
-# Template
+# Bitcoin API Setup using SQLite Script
 # #############################################################################
 
+API_KEY = 'bdbd5008-53de-4bf9-b9b8-c1edb2c6afb0'
+headers = {'X-CMC_PRO_API_KEY': API_KEY,'Accept': 'application/json'}
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+params = {'limit': 1,'convert': 'USD'}
 
-class Template:
-    """
-    Brief imperative description of what the class does in one line, if needed.
-    """
+DATEFMT = "%Y-%m-%d-%H-%M-%S"
 
-    def __init__(self):
-        pass
+# Load Data
+data = sqlite_utils.fetchHistoricalBTC(ticker="BTC-USD", period='15y', interval='1d')
 
-    def method1(self, arg1: int) -> None:
-        """
-        Brief imperative description of what the method does in one line.
+# Storing Data into SQLite
+sqlite_utils.storeData(data=data, db="./btcDaily.db")
 
-        You can elaborate more in the method docstring in this section, for
-        e.g. explaining the formula/algorithm. Every method/function should
-        have a docstring, typehints and include the parameters and return as
-        follows:
+# Query Data from SQLite
+query = "SELECT * FROM btc_daily_stats"
+data = sqlite_utils.fetchDB(query=query, db="./btcDaily.db")
 
-        :param arg1: description of arg1
-        :return: description of return
-        """
-        # Code bloks go here.
-        # Make sure to include comments to explain what the code is doing.
-        # No empty lines between code blocks.
+# Live BTC Price
+url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+API_KEY = 'bdbd5008-53de-4bf9-b9b8-c1edb2c6afb0'
+btcLiveData, btcAuxData = sqlite_utils.liveBTC(url=url, API_KEY=API_KEY)
+
+# Inserting Live Data into SQLite
+sqlite_utils.addDatapoint(liveData=btcLiveData, db="./btcDaily.db")
+
+# #############################################################################
+# Bitcoin Analysis using SQLite Script
+# #############################################################################
+# 
+
+query = "SELECT * FROM btc_daily_stats"
+data = sqlite_utils.fetchDB(query=query, db="./btcDaily.db")
+data['date'] = pd.to_datetime(data['date'], format=DATEFMT)
+
+# Bitcoin Open Price
+plt.figure(figsize=(15,5), dpi=100)
+sns.lineplot(data=data, x='date', y='open')
+sns.scatterplot(x=[data.iloc[-1]['date']], y=[data.iloc[-1]['open']], s=500, marker='*', color='red', label='Current BTC Price') 
+plt.grid(True)
+plt.xlabel("Date")
+plt.ylabel("Opening Price")
+plt.title("BTC Opening price across time.")
+plt.show(block=False)
+
+## Moving Average
+query = '''SELECT 
+    date,
+    open,
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_2d_open, --Adding 2 Day Moving Average
+    
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_30d_open, --Adding 30 Day Moving Average
+    
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 119 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_120d_open --Adding 120 Day Moving Average
+FROM btc_daily_stats
+ORDER BY date;
+'''
+dataMA = sqlite_utils.fetchDB(query=query, db="./btcDaily.db")
+dataMA['date'] = pd.to_datetime(dataMA['date'], format=DATEFMT)
+
+plt.figure(figsize=(15,5), dpi=100)
+sns.lineplot(data=dataMA, x='date', y='open', label='Open Price', color='royalblue')
+sns.lineplot(data=dataMA, x='date', y='ma_2d_open', label='2day MA Open Price', color='tomato')
+sns.lineplot(data=dataMA, x='date', y='ma_30d_open', label='30day MA Open Price', color='orange')
+sns.lineplot(data=dataMA, x='date', y='ma_120d_open', label='120day MA Open Price', color='forestgreen')
+
+sns.scatterplot(x=[dataMA.iloc[-1]['date']], y=[dataMA.iloc[-1]['open']], s=500, marker='*', color='red', label='Current BTC Price') 
+plt.grid(True)
+plt.xlabel("Date")
+plt.ylabel("Opening Price")
+plt.title("BTC Opening price across time.")
+plt.show(block=False)
+
+## Moving Average - Data Range
+dateStart, dateEnd = '2024-12-24-00-00-00', '2025-04-15-00-00-00'
+
+query = f'''SELECT 
+    date,
+    open,
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_2d_open,
+    
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_30d_open,
+    
+    ROUND(AVG(open) OVER (
+        ORDER BY date 
+        ROWS BETWEEN 119 PRECEDING AND CURRENT ROW
+    ), 2) AS ma_120d_open
+FROM btc_daily_stats
+WHERE date BETWEEN '{dateStart}' AND '{dateEnd}' --Adding Date Range
+ORDER BY date;
+'''
+
+dataMA = sqlite_utils.fetchDB(query=query, db="./btcDaily.db")
+dataMA['date'] = pd.to_datetime(dataMA['date'], format=DATEFMT)
+
+plt.figure(figsize=(15,5), dpi=100)
+sns.lineplot(data=dataMA, x='date', y='open', label='Open Price', color='royalblue')
+sns.lineplot(data=dataMA, x='date', y='ma_2d_open', label='2day MA Open Price', color='tomato')
+sns.lineplot(data=dataMA, x='date', y='ma_30d_open', label='30day MA Open Price', color='orange')
+sns.lineplot(data=dataMA, x='date', y='ma_120d_open', label='120day MA Open Price', color='forestgreen')
+
+sns.scatterplot(x=[dataMA.iloc[-1]['date']], y=[dataMA.iloc[-1]['open']], s=400, marker='*', color='red', label='Current BTC Price') 
+plt.grid(True)
+plt.xlabel("Data")
+plt.ylabel("Opening Price")
+
+plt.title(f"BTC Opening price from {dateStart[:10]} to {dateEnd[:10]}")
+plt.show(block=False)
 
 
-def template_function(arg1: int) -> None:
-    """
-    Brief imperative description of what the function does in one line.
-
-    You can elaborate more in the function docstring in this section, for e.g.
-    explaining the formula/algorithm. Every function should have a docstring,
-    typehints and include the parameters and return as follows:
-
-    :param arg1: description of arg1
-    :return: description of return
-    """
-    # Code bloks go here.
-    # Make sure to include comments to explain what the code is doing.
-    # No empty lines between code blocks.
-    pass
+plt.show()
