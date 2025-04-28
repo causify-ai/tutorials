5913f3ae9cd679956c3fbf564b2f12be8ab42cca
Adwait Gaur <132753601+Adwait197@users.noreply.github.com>
2025-04-28 19:05:02 -0400

    Created a docker image

--- DATA605/Spring2025/projects/TutorTask121_Spring2025_Real-Time_Bitcoin_Analysis_with_SQLAlchemy/docker_data605_style/docker_bash.sh
+++ DATA605/Spring2025/projects/TutorTask121_Spring2025_Real-Time_Bitcoin_Analysis_with_SQLAlchemy/docker_data605_style/docker_bash.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-docker run -it --rm \
-  -p 8888:8888 \
-  --name data605_project_container \
-  data605_project_image \
-  bash
+REPO_NAME=umd_data605
+IMAGE_NAME=umd_data605_template
+FULL_IMAGE_NAME=$REPO_NAME/$IMAGE_NAME
 
+docker image ls $FULL_IMAGE_NAME
+
+CONTAINER_NAME=$IMAGE_NAME
+docker run --rm -ti \
+    --name $CONTAINER_NAME \
+    --entrypoint /data \    
+    -p 8888:8888 \
+    -v $(pwd):/data \
+    $FULL_IMAGE_NAME
