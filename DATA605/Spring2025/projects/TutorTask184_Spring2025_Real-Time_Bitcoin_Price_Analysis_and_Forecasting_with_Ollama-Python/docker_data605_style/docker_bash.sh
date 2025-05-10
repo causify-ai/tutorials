c123e036cfbc9bfdd01b36aa9449d3c3b54c819e
Vikranth Reddimasu <vikranthreddimasu@Viks-MacBook-Air.local>
2025-05-09 21:26:36 -0400

    chore: update docker build/run scripts

--- DATA605/Spring2025/projects/TutorTask184_Spring2025_Real-Time_Bitcoin_Price_Analysis_and_Forecasting_with_Ollama-Python/docker_data605_style/docker_bash.sh
+++ DATA605/Spring2025/projects/TutorTask184_Spring2025_Real-Time_Bitcoin_Price_Analysis_and_Forecasting_with_Ollama-Python/docker_data605_style/docker_bash.sh
@@ -8,7 +8,7 @@ docker image ls $FULL_IMAGE_NAME
 
 CONTAINER_NAME=$IMAGE_NAME
 docker run --rm -ti \
-    --name $CONTAINER_NAME \
-    -p 8888:8888 \
-    -v $(pwd):/data \
-    $FULL_IMAGE_NAME
+  --name $CONTAINER_NAME \
+  -p 8888:8888 \
+  -v $(pwd):/data \
+  $FULL_IMAGE_NAME
