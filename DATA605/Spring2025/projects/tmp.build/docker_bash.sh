6bdc33f8623248578f458cb1ccdb5d4fd5999299
Riya Disawal <riyayrd@Riyas-MacBook-Air.local>
2025-05-13 17:02:15 -0400

    TutorTask99_Spring2025_Real-Time_Bitcoin_Data_Processing_with_Datasette

--- DATA605/Spring2025/projects/tmp.build/docker_bash.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -p 8888:8888 -p 8001:8001 -v $(pwd):/app bitcoin_env bash
--- DATA605/Spring2025/projects/tmp.build/docker_jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -p 8888:8888 -v $(pwd):/app bitcoin_env jupyter notebook --ip=0.0.0.0 --allow-root --no-browser
