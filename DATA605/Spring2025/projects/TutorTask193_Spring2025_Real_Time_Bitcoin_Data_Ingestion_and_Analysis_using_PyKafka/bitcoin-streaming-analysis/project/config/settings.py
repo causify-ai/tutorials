9efe85270684c3a17f083a23c8647c6b2a89f436
amansahu205 <53536739+amansahu205@users.noreply.github.com>
2025-05-18 23:21:47 -0400

    Live plotting fixed

--- DATA605/Spring2025/projects/TutorTask193_Spring2025_Real_Time_Bitcoin_Data_Ingestion_and_Analysis_using_PyKafka/bitcoin-streaming-analysis/project/config/settings.py
+++
@@ -1,4 +0,0 @@
-KAFKA_BROKER = "localhost:9092"
-KAFKA_TOPIC = "btc_prices"
-COINGECKO_API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-FETCH_INTERVAL = 5  # in seconds
--- DATA605/Spring2025/projects/TutorTask193_Spring2025_Real_Time_Bitcoin_Data_Ingestion_and_Analysis_using_PyKafka/bitcoin-streaming-analysis/project/utils/helpers.py
+++
@@ -1,3 +0,0 @@
-def format_timestamp(ts):
-    from datetime import datetime
-    return datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
