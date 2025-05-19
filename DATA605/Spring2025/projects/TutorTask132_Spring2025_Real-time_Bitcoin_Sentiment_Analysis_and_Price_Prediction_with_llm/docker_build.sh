f7f68199f6443450fea12fbb12f2369f4ec19325
rishikathakre <rishikathakre11@gmail.com>
2025-05-18 21:21:13 -0400

    Removed unwanted files after cleanup

--- DATA605/Spring2025/projects/TutorTask132_Spring2025_Real-time_Bitcoin_Sentiment_Analysis_and_Price_Prediction_with_llm/docker_build.sh
+++ DATA605/Spring2025/projects/TutorTask132_Spring2025_Real-time_Bitcoin_Sentiment_Analysis_and_Price_Prediction_with_llm/docker_build.sh
@@ -4,7 +4,7 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 source $GIT_ROOT/docker_common/utils.sh
 
 REPO_NAME=umd_data605
-IMAGE_NAME=umd_data605_template
+IMAGE_NAME=bitcoin_cli_project
 
 # Build container.
 export DOCKER_BUILDKIT=1
--- DATA605/Spring2025/projects/TutorTask132_Spring2025_Real-time_Bitcoin_Sentiment_Analysis_and_Price_Prediction_with_llm/docker_exec.sh
+++ DATA605/Spring2025/projects/TutorTask132_Spring2025_Real-time_Bitcoin_Sentiment_Analysis_and_Price_Prediction_with_llm/docker_exec.sh
@@ -4,6 +4,6 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 source $GIT_ROOT/tutorial_github_simple/docker_common/utils.sh
 
 REPO_NAME=umd_data605
-IMAGE_NAME=umd_data605_template
+IMAGE_NAME=bitcoin_cli_project
 
 exec_container
